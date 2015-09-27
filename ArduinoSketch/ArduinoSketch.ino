b411476958a04b722c0f605a0a01a2889e24f064
philenius <philippus-perez@web.de>
2015-09-27 20:21:22 +0200

    improved fading of puslations by fading to black instead of turning off LED by LED; added method randomBlueFlash which looks like a thunderstorm

--- ArduinoSketch/ArduinoSketch.ino
+++ ArduinoSketch/ArduinoSketch.ino
@@ -15,29 +15,61 @@ void setup() {
 }
 
 void loop() {
-  
+
+  int r = random(0,3);
+  switch (r) {
+    case 0:  pulsateRed(); break;
+    case 1:  pulsateGreen(); break;
+    case 2:  pulsateBlue(); break;
+  }
+
   for(int i = 0; i < random(1,10); i++) {
     pulsate();
   }
-  
+ 
   delay(2000);
-  
+
   for(int i = 0; i < random(0,10); i++) {
     fullSpectrumWipe();
   }
 
   delay(2000);
   
+  randomBlueFlash();
+  
+  delay(2000);
+  
   for(int i = 0; i < random(0,10); i++) {
-    pulsateBlue();
+    pulsateRandomBlue();
   }
   
   for(int i = 0; i < random(0,10); i++) {
     pulsatePink();
   }
-
-  delay(2000);
   
+  delay(2000);
+}
+
+void randomBlueFlash()
+{
+  for(int i = 0; i < 50; i++) {
+    int blue = random(0,256);
+    int red = random(0,100);
+    
+    setColorForAll(pixels.Color(red,0,blue));
+    delay(random(50,150));
+    setColorForAll(pixels.Color(0,0,0));
+    delay(random(50,700));
+  }
+}
+
+// pulsation in pink
+void pulsatePink() {
+  delay(1000);
+  colorWipe(pixels.Color(255,0,154), 25);
+  delay(500);
+  colorWipe(pixels.Color(0,0,0), 55);
+  delay(200);
 }
 
 // pulsation in random color
@@ -46,16 +78,16 @@ void pulsate() {
   int g = random(0,256);
   int b = random(0,256);
   delay(1000);
-  colorWipe(pixels.Color(r,g,b), 55);
+  colorWipe(pixels.Color(r,g,b), 50);
   delay(100);
-  colorWipe(pixels.Color(0,0,0), 65);
+  colorWipe(pixels.Color(0,0,0), 55);
   delay(100);
 }
 
 // pulsation in random blue with random delay
-void pulsateBlue() {
+void pulsateRandomBlue() {
   int wait = random(100,1000);
-  int tempo = random(35,55);
+  int tempo = random(45,55);
   int r = random(0,207);
   int g = random(120,204);
   delay(wait);
@@ -65,18 +97,54 @@ void pulsateBlue() {
   delay(wait);
 }
 
-// pulsation in pink
-void pulsatePink() {
-  delay(1000);
-  colorWipe(pixels.Color(255,0,154), 20);
-  delay(500);
-  colorWipe(pixels.Color(0,0,0), 55);
-  delay(200);
+// ##########################################################################################################
+// Methods which let the lamp pulsate in ether red(255,0,0), green(0,255,0) or blue(0,0,255) fading to black.
+// pulsation in red
+void pulsateRed() {
+  int wait = random(1,20); 
+  for(int i = 0; i < 255; i++) {
+    setColorForAll(pixels.Color(i,0,0));
+    delay(wait);
+  }
+  for(int i = 255; i > 0; i--) {
+    setColorForAll(pixels.Color(i,0,0));
+    delay(wait);
+  }
 }
+
+// pulsation in blue
+void pulsateBlue() {
+  int wait = random(1,20); 
+  for(int i = 0; i < 255; i++) {
+    setColorForAll(pixels.Color(0,0,i));
+    delay(wait);
+  }
+  for(int i = 255; i > 0; i--) {
+    setColorForAll(pixels.Color(0,0,i));
+    delay(wait);
+  }
+}
+
+// pulsation in green
+void pulsateGreen() {
+  int wait = random(1,20); 
+  for(int i = 0; i < 255; i++) {
+    setColorForAll(pixels.Color(0,i,0));
+    delay(wait);
+  }
+  for(int i = 255; i > 0; i--) {
+    setColorForAll(pixels.Color(0,i,0));
+    delay(wait);
+  }
+}
+// ##########################################################################################################
 
 // full wipe using the complete spectrum
 void fullSpectrumWipe() {
-  colorWipe(pixels.Color(255,0,0),50);
+  for(int i = 0; i < 255; i++) {
+    setColorForAll(pixels.Color(i,0,0));
+    delay(20);
+  }
   for(int g = 0; g < 255; g++) {
     colorWipe(pixels.Color(255,g,0),1);
   }
@@ -95,13 +163,25 @@ void fullSpectrumWipe() {
   for(int b = 255; b > 0; b--) {
     colorWipe(pixels.Color(255,0,b),1);
   }
+  for(int i = 255; i > 0; i--) {
+    setColorForAll(pixels.Color(i,0,0));
+    delay(20);
+  }
 }
 
-// method for setting colors of the ring with delay between each LED
+// Method which sets the colors of the ring with a given delay between each LED.
 void colorWipe(uint32_t c, uint8_t wait) {
   for(uint16_t i=0; i<NUMPIXELS; i++) {
     pixels.setPixelColor(i,c);
     pixels.show();
     delay(wait);
   }
+}
+
+// Method which sets all LEDs at the same time to the same color.
+void setColorForAll(uint32_t color) {
+  for(uint16_t i=0; i<NUMPIXELS; i++) {
+    pixels.setPixelColor(i,color);
+  }
+  pixels.show();  
 }
